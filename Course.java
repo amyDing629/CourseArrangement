f56a6882d5eae601685155464e4381b02dfb2ac8
Jian Hong <jianhong.guo@mail.utoronto.ca>
2021-01-05 19:56:01 -0500

    constructor for course class

---
+++ Course.java
@@ -0,0 +1,24 @@
+import List;
+
+public class Course{
+
+private String courseID;
+private List<List> time;
+private String instructor;
+private List<String> prerequisite;
+private List<String> exclusion;
+private String department;
+
+    /**
+     * @param courseID the id of the course, such as "CSC108"
+     * @param time a list contains all the class time for the course
+     * @param instructor
+     * @param prerequisite
+     * @param exclusion
+     * @param department
+     */
+public Course(String courseID, List<List> time, String instructor, List<String>
+        prerequisite, List<String> exclusion, String department){
+
+}
+}
