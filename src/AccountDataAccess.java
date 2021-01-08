fee69d8cdc6adf898120ecadd3693eab335c76d6
yiyun Ding <amydingyiyun@gmail.com>
2021-01-07 21:51:32 -0500

    DataAccess + accountManager

---
+++ src/AccountDataAccess.java
@@ -0,0 +1,54 @@
+import java.util.List;
+import java.util.UUID;
+
+public class AccountDataAccess implements iDataAccess {
+    @Override
+    public List<Object> getList() {
+        return null;
+    }
+
+    @Override
+    public Object getObject(String name) {
+        return null;
+    }
+
+    @Override
+    public Object getObject(UUID uuid) {
+        return null;
+    }
+
+    @Override
+    public void addObject(Object o) {
+
+    }
+
+    @Override
+    public boolean hasObject(Object o) {
+        return false;
+    }
+
+    @Override
+    public void removeObject(String o) {
+
+    }
+
+    @Override
+    public void removeObject(UUID o) {
+
+    }
+
+    @Override
+    public void updateSer() {
+
+    }
+
+    @Override
+    public void deSerialize() {
+
+    }
+
+    @Override
+    public void setList(List<Object> list) {
+
+    }
+}
